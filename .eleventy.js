2a45d91666fc919e856e6005ba6614e7e029951d
Vincent LE GOFF <g_n_s@hotmail.fr>
2019-07-06 19:12:29 +0200

    Chore (#1)
    * sitemap addition
    
    * add link
    
    * title fix
    
    * add postcss autoprefixer
    
    * header clean

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,17 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 const CleanCSS = require("clean-css");
+const { DateTime } = require("luxon");
+const postcss = require("postcss");
+const autoprefixer = require("autoprefixer");
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addFilter("htmlDateString", dateObj => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+  eleventyConfig.addFilter("postCSS", function(code) {
+    return postcss([autoprefixer]).process(code).css;
+  });
+
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
@@ -14,4 +26,3 @@ module.exports = function(eleventyConfig) {
     }
   };
 };
-
