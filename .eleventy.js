d8204731a54493559f83be50011c89418fec8208
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-07-06 19:04:36 +0200

    add postcss autoprefixer

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,17 @@
-
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const CleanCSS = require("clean-css");
 const { DateTime } = require("luxon");
+const postcss = require("postcss");
+const autoprefixer = require("autoprefixer");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  eleventyConfig.addFilter("htmlDateString", dateObj => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
+  eleventyConfig.addFilter("postCSS", function(code) {
+    return postcss([autoprefixer]).process(code).css;
+  });
+
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
@@ -21,4 +26,3 @@ module.exports = function(eleventyConfig) {
     }
   };
 };
-
