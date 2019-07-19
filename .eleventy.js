03973f05d5bee52012c0fd390ed36d5a81d5d70b
Vincent LE GOFF <g_n_s@hotmail.fr>
2019-07-19 20:56:52 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -45,6 +45,7 @@ module.exports = function(eleventyConfig) {
   });
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/.nojekyll");
   return {
     pathPrefix: "/",
     dir: {
