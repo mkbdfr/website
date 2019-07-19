eaef8e36e4873b238540db6e111ee97f8fe7c118
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-07-19 19:23:15 +0200

    fix pathprefix

--- .eleventy.js
+++ .eleventy.js
@@ -46,7 +46,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   return {
-    pathPrefix: "/mkfr/",
+    pathPrefix: "/",
     dir: {
       input: "./src",
       includes: "includes",
